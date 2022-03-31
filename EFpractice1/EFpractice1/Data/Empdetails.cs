b5dfc31f4ece4421657880449c10ede6b1155757
mashi <mashi@MALLIKARJUN>
2022-04-01 00:09:54 +0530

    Add project files.

---
+++ EFpractice1/EFpractice1/Data/Empdetails.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace EFpractice1.Data
+{
+    public class Empdetails
+    {
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ EFpractice1/EFpractice1/Data/practice1DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFpractice1.Data
+{
+    public class practice1DbContext : DbContext
+    {
+
+        public practice1DbContext(DbContextOptions<practice1DbContext> options)
+            :base(options)
+        {
+                
+        }
+        DbSet<Empdetails> tblemployeeDetails { get; set; }
+    }
+}
